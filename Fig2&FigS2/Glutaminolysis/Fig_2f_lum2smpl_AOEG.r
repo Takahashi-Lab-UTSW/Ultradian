bb6fa452b18ed116a4813ded301e08f808c0c740
Shuzhang Yang <67648688+shuzhangyang@users.noreply.github.com>
2022-10-19 12:51:40 -0500

    Add files via upload

---
+++ Fig2&FigS2/Glutaminolysis/Fig_2f_lum2smpl_AOEG.r
@@ -0,0 +1,122 @@
+path<-"C:/Users/SHU/Documents/Doc/UltradianCycle/UC-Plots/All_Figs/Figs_D/Fig2/Glutaminolysis/"
+
+setwd(paste(path))
+
+# folders = list.dirs('.', full.names=FALSE, recursive=FALSE)
+# read csv data exported from Lumicycle Analysis
+samp.AOEG<-c("Vehicle", "AOA","EGCG","AOA+EGCG")  ##################################### provide sampe name
+plotname<-"p_AOEG"   ############################################ plot name
+dats<-list()
+nrows<-c()
+files = list.files(pattern="*.csv")
+# file1<-files[grepl("_AOA",files)]    ###############################
+# file0<-files[grepl("Vehicle", files)]
+# file<-c(file0,file1)
+
+n<-length(samp.AOEG)
+clrs<-c("blue", "orange","green", "red", "violet","cyan") # 6 colours
+# clrs<-c("blue", "red", "orange","violet","cyan")
+c<-clrs[1:n]
+
+# read raw data exported from Lumicycle Analysis
+for (i in 1:n){
+  df<-read.csv(files[grepl(paste("_",samp.AOEG[i],"_",sep=""),files, fixed = TRUE)], skip=3, col.names = c("Date","Time",	"Days",	"CountsPerSec",	"Baseline")) 
+  lum<-df[,"CountsPerSec"]
+  day<-df$Days-df$Days[1]   # normalized time in days
+  
+  # # detrend lum by subtracting the loess line, then shift back ############################## detrend
+  # dat<-cbind(day,lum)
+  # loess_fit <- loess(lum ~ day, data.frame(dat))
+  # lum<-lum-predict(loess_fit)+mean(predict(loess_fit))
+  
+  dats[[i]]<-cbind(day,lum)
+  nrows<-c(nrows,nrow(df))
+}
+
+# put data in one dataframe and subset
+nr<-min(nrows)   # number of rows to plot
+dat<-data.frame()
+for (i in 1:n){
+  if (i==1){
+    dat<-dats[[i]][1:nr,]
+  }else{
+    dat<-cbind(dat,dats[[i]][1:nr,2])
+  }
+}
+colnames(dat)<-c("Days",samp.AOEG)
+
+# subset data
+dat<-dat[dat[,1]<=3.5,]
+
+# normalize data to mean of the 1st day
+if (n>1){
+  dat0<-sweep(dat[,-1], 2, colMeans(dat[dat[,1]<0.5,-1]), FUN="/") #normalize to mean of the 1st day
+  dat0<-cbind(dat[,1],dat0)
+  colnames(dat0)<-c("Days",samp.AOEG)
+}else{
+  lum.norm<-dat[,2]/mean(dat[,2])
+  dat0<-cbind(dat[,1],lum.norm)
+  colnames(dat0)<-c("Days",samp.AOEG)
+}
+
+# make long form data
+library("reshape2")
+dat0<-data.frame(dat0)   # wide form data
+dat1 <- melt(dat0, id.vars="Days", variable.name="Sample", value.name="Luminescence")
+library(dplyr)
+rg <- range(dat1$Luminescence)%>%diff
+
+#plot
+library(ggplot2)
+library(scales)
+
+scaleFUN <- function(x) sprintf("%.1f", x)  # digits
+
+p <- ggplot(data=dat1, aes(x=Days, y=Luminescence, group = Sample, colour = factor(Sample, labels=samp.AOEG))) +  
+  geom_line(linetype=1,size=0.2)+
+  ylab("Luminescence") +
+  xlab("Days") + 
+  theme_classic() +       # Removes gridlines & background
+  theme(axis.title = element_text(face = "plain", color = "black", size = 6), 
+        # axis.title.x = element_text(face = "plain", color = "black", size = 8), 
+        axis.text = element_text(face = "plain", color = "black", size = 5), 
+        # axis.text.x = element_text(face = "plain", color = "black", size = 8), 
+        # axis.line = element_blank(),
+        axis.line = element_line(colour = "black", size = 0.1),
+        axis.ticks = element_line(colour = "black", size = 0.1),
+        axis.ticks.length = unit(1., "pt"),
+        # panel.border = element_rect(colour = "black", fill=NA, size=0.1),
+        plot.margin = margin(1, 1, 1,1, "pt"),
+        legend.position = c(0.5,0.95),
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        legend.text = element_text(size=4.5,margin = margin(t=.1,b=.1, unit = "line")),
+        legend.key.height = unit(0.3,"line"),
+        legend.key.width = unit(0.2, "line"),
+        legend.spacing.x = unit(0.1, "line"),
+        # legend.spacing.y = unit(0.05, "line")
+        )+
+  guides(colour = guide_legend(nrow = 2))+
+  scale_color_manual(values=c)+
+  scale_y_continuous(expand=c(0.1,0.1), limits=c(0.1,2.4), breaks=pretty_breaks(n = 4), labels=scaleFUN) # set y-axis labels
+
+# # Add text on a specific positions:
+# p<- p + annotate("text", x = c(2,4.5), y = c(20,25), label = c("label 1", "label 2") , color="orange", size=5 , angle=45, fontface="bold")
+# # Add rectangles
+p<- p + annotate("rect", xmin=0.7, xmax=2.15, ymin=0.1 , ymax=0.1+2.3/25, alpha=0.25, size=0, color="black", fill="black")
+# # Add segments
+# p<- p + annotate("segment", x = 1, xend = 3, y = 25, yend = 15, colour = "purple", size=3, alpha=0.6)
+# Add arrow
+# p<- p + annotate("segment", x = 1.7, xend = 1.7, y = 0.35, yend = 0.45, colour = "black", size=0.25, alpha=1, arrow=arrow(length=unit(0.05,"npc")))
+
+if (n==1){
+  p<-p + theme(legend.position="none") + 
+    annotate("text", label=samp.AOEG, color="black", x=max(dat1$Days), y=max(dat1$Luminescence), hjust=1, vjust=0.75, size=2.8) # label/legend for single sample
+}
+assign(plotname,p)
+
+# # output pdf
+ggsave(filename=paste(plotname,"pdf",sep="."), plot=get(plotname), useDingbats=F, width=1.3, height=.75)
+
+
+
